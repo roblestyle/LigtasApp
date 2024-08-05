7a564edfa98dc3afba9c4e188a480b81853ea8d0
roblestyle <133630940+roblestyle@users.noreply.github.com>
2024-08-05 21:23:31 +0800

    new

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  // output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // },
   // basePath: '/commandcenter/ligtas/app',
   // assetPrefix: '/commandcenter/ligtas/app/', 
 };
